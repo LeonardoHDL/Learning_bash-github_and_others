82ee5dc329551ac3e762c07f2965f6b4cd8cb60d
LeonardoHDL <85359743+LeonardoHDL@users.noreply.github.com>
2023-03-08 21:12:47 +0000

    popular_terminal_commands

---
+++ popular_terminal_commands.sh
@@ -0,0 +1,7 @@
+#every linux bash command or function has the same structure
+#which is :
+#command -option target
+cal #shows me a graphical calendar
+date #also shows the date in txt format
+df -h #tells me the size of the files located in the cwd
+wc -l #tell me the number of lines of a file
