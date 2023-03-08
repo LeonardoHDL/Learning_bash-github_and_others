a02e9676573c3bd4c6e2d88db11d809dcfe55c48
LeonardoHDL <85359743+LeonardoHDL@users.noreply.github.com>
2023-03-08 23:33:04 +0000

    add new tip about usage of these '<>'

--- popular_terminal_commands.sh
+++ popular_terminal_commands.sh
@@ -4,9 +4,9 @@
 cal #shows me a graphical calendar
 date #also shows the date in txt format
 df -h #tells me the size of the files located in the cwd
-wc -l #tell me the number of lines of a file, why does it always tells me n_lines -1?
+wc -l <filename>  #tells me the number of lines of a file, why does it always tells me n_lines -1?
 
-
+#<> replaces means that the file especification does not require ''
 #we are adding some new lines just to check that this is working correctly
 
 
