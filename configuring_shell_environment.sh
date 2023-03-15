1116933255dddb3e35be74a015ea2384d2fa425d
LeonardoHDL <85359743+LeonardoHDL@users.noreply.github.com>
2023-03-15 23:39:36 +0000

    Lesson of configuring shell environments
    we installed zsh and saw that it contains things that bash doesn't

---
+++ configuring_shell_environment.sh
@@ -0,0 +1,17 @@
+#you can put a configuration file everytime 
+#you log into a computer or a cluster so
+#so you can access to everything with the configs
+#that you have developed
+ 
+
+#there's a form so that we can store things
+#on variables , and the call them directly from the
+#CLI
+#this is with the command export
+export trial='2134141'
+#and for to print it i'd like 
+echo $trial
+
+#the idea behind this is that we can use
+#the same thing on our bashrc file so we can acces
+#anytime 
